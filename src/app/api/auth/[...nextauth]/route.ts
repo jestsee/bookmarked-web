e5f63ed120a0c3e76eb5350ffbe2a85786d2833c
Jesica <jestsee@gmail.com>
2024-02-08 13:01:44 +0700

    feat: setup trpc client

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,12 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 
 import db from "@/database/client";
 
 const handler = NextAuth({
-  adapter: DrizzleAdapter(db),
+  adapter: DrizzleAdapter(db) as NextAuthOptions["adapter"],
   providers: [
     CredentialsProvider({
       name: "Email",
