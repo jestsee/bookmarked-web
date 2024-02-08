8e1ea030a985a032869ae5c7fecbae5477223daa
Jesica <jesica@btpn.com>
2024-02-08 13:01:44 +0700

    feat: setup trpc client

--- src/database/client.ts
+++ src/database/client.ts
@@ -4,5 +4,4 @@ import { drizzle } from "drizzle-orm/neon-http";
 const sql = neon(process.env.DATABASE_URL);
 const db = drizzle(sql);
 
-export default db
-// const result = await db.select().from(...);
+export default db;
--- src/trpc-client/trpc.ts
+++ src/trpc-client/trpc.ts
@@ -1,3 +1,5 @@
+import { createTRPCReact } from '@trpc/react-query';
+
 import { AppRouter } from "@/app/api/trpc/trpc.router";
 
-// export const trpc = createTRPCReact<AppRouter>();
+export const trpc = createTRPCReact<AppRouter>();
