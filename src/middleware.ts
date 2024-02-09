0a30e79a02fe1f6f7da84c7db4e9687128455815
Jesica <jestsee@gmail.com>
2024-02-09 23:58:33 +0700

    feat: update middleware for non auth routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,32 @@
-export { default } from "next-auth/middleware";
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
 
-export const config = {
-  matcher: ["/((?!sign-in|sign-up|api).*)"],
-};
+const nonAuthRoutes = ["sign-up", "api/auth/signin"];
+
+export default async function middleware(
+  req: NextRequest,
+  event: NextFetchEvent,
+) {
+  const token = await getToken({ req });
+  const isAuthenticated = !!token;
+  const { pathname } = req.nextUrl;
+
+  const isAccessingNonAuthRoutes = nonAuthRoutes.some((prefix) =>
+    pathname.startsWith(`/${prefix}`),
+  );
+
+  if (!isAuthenticated && isAccessingNonAuthRoutes) {
+    return NextResponse.next();
+  }
+
+  if (isAuthenticated && isAccessingNonAuthRoutes) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return withAuth(req as NextRequestWithAuth, event);
+}
+
+// export const config = {
+//   matcher: ["/((?!sign-up|api).*)"],
+// };
