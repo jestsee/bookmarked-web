5153bba72802c14712e4162be7abbefc31c11845
Jesica <jesica@btpn.com>
2024-02-25 16:12:56 +0700

    feat: display auth providers

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,15 @@
+import { getProviders } from "next-auth/react";
+
 import LoginForm from "./login-form";
 
-const SignIn = () => {
+const SignIn = async () => {
+  const providers = await getProviders();
+
   return (
     <div>
       <h1>Sign in</h1>
       <h3>Welcome back! Enter your email and password below to sign in.</h3>
-      <LoginForm />
+      <LoginForm {...{ providers }} />
     </div>
   );
 };
