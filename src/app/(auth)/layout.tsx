547e5a3b4aeec454601bd8f8faa51d2b29d2eb22
Jesica <13519011@std.stei.itb.ac.id>
2024-03-02 13:28:32 +0700

    feat: refactor auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -13,7 +13,7 @@ import {
 import { Separator } from "@/components/ui/separator";
 import { LayoutProps } from "@/types";
 
-import AuthProviderButton from "./auth-provider-button";
+import AuthProviderButton from "./components/auth-provider-button";
 
 interface Props extends LayoutProps {
   header: ReactNode;
@@ -24,37 +24,35 @@ const AuthenticationLayout = async ({ children, header, footer }: Props) => {
   const providers = await getProviders();
 
   return (
-    <div className="absolute left-1/2 top-1/2 w-[420px] -translate-x-1/2 -translate-y-1/2">
-      <Card>
-        <CardHeader className="text-center">
-          {header}
-          <CardTitle className="mb-1 mt-4 font-bold">Welcome Back</CardTitle>
-          <CardDescription>
-            Don&apos;t have an account yet?&nbsp;
-            <Link
-              className="font-semibold underline-offset-2 hover:underline"
-              href="/sign-up"
-            >
-              Sign up
-            </Link>
-          </CardDescription>
-        </CardHeader>
-        <CardContent>{children}</CardContent>
-        <Separator className="mb-2 gap-3">
-          <p className="text-sm uppercase">or</p>
-        </Separator>
-        <CardFooter className="space-x-3">
-          {Object.values(providers ?? []).map((provider) => {
-            return (
-              provider.id !== "credentials" && (
-                <AuthProviderButton {...{ provider }} />
-              )
-            );
-          })}
-        </CardFooter>
-        {footer}
-      </Card>
-    </div>
+    <Card className="absolute left-1/2 top-1/2 w-[420px] -translate-x-1/2 -translate-y-1/2">
+      <CardHeader className="text-center">
+        {header}
+        <CardTitle className="mb-1 mt-4 font-bold">Welcome Back</CardTitle>
+        <CardDescription>
+          Don&apos;t have an account yet?&nbsp;
+          <Link
+            className="font-semibold underline-offset-2 hover:underline"
+            href="/sign-up"
+          >
+            Sign up
+          </Link>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      <Separator className="mb-2 gap-3">
+        <p className="text-sm uppercase">or</p>
+      </Separator>
+      <CardFooter className="space-x-3">
+        {Object.values(providers ?? []).map((provider) => {
+          return (
+            provider.id !== "credentials" && (
+              <AuthProviderButton key={provider.id} {...{ provider }} />
+            )
+          );
+        })}
+      </CardFooter>
+      {footer}
+    </Card>
   );
 };
 
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,9 +1,54 @@
-import React from "react";
+"use client";
 
-import RegisterForm from "./register-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
-const SignUp = () => {
-  return <RegisterForm />;
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import { CreateUserInput, createUserSchema } from "@/server/auth/auth.schema";
+import { trpc } from "@/trpc-client/trpc";
+
+import CustomAlert from "../components/custom-alert";
+import CustomForm from "../components/custom-form";
+import { FieldConfig } from "../type";
+
+const fieldConfigs: FieldConfig<CreateUserInput>[] = [
+  { name: "name", label: "Name", placeholder: "John Doe" },
+  { name: "email", label: "Email", placeholder: "example@gmail.com" },
+  { name: "password", label: "Password", isPassword: true },
+  { name: "passwordConfirm", label: "Confirm password", isPassword: true },
+];
+
+const RegisterForm = () => {
+  const form = useForm<CreateUserInput>({
+    resolver: zodResolver(createUserSchema),
+    defaultValues: { email: "", name: "", password: "", passwordConfirm: "" },
+  });
+  const { handleSubmit } = form;
+  const { mutate, error, isPending } = trpc.registerUser.useMutation({
+    onSuccess() {
+      toast.success("Successfully signed up!");
+    },
+  });
+
+  const onSubmit = handleSubmit((values) => mutate(values));
+
+  return (
+    <Form {...form}>
+      <form {...{ onSubmit }} className="flex flex-col gap-y-4">
+        {error && <CustomAlert message={error.message} />}
+        {fieldConfigs.map((fieldConfig) => (
+          <CustomForm key={fieldConfig.name} {...{ form, ...fieldConfig }} />
+        ))}
+        <p>
+          Already have an account? <Link href="/sign-in">Sign in</Link>
+        </p>
+        <Button loading={isPending}>Sign up</Button>
+      </form>
+    </Form>
+  );
 };
 
-export default SignUp;
+export default RegisterForm;
