d76dfbf7cfeb9853148004bff01ccde3c6948cd7
Jesica <13519011@std.stei.itb.ac.id>
2024-03-03 14:22:02 +0700

    feat: add loading state when signing out

--- src/app/(authenticated)/_navbar/sign-out.tsx
+++ src/app/(authenticated)/_navbar/sign-out.tsx
@@ -1,11 +1,27 @@
 "use client";
 
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { signOut } from "next-auth/react";
+import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
 
 const SignOut = () => {
-  return <Button onClick={() => signOut()}>Sign Out</Button>;
+  const router = useRouter();
+  const { mutate, isPending } = useMutation({
+    mutationFn: async () => signOut({ redirect: false }),
+    onSuccess: () => {
+      toast.success("Successfully signed out");
+      router.push("/sign-in");
+    },
+  });
+
+  return (
+    <Button loading={isPending} onClick={() => mutate()}>
+      Sign Out
+    </Button>
+  );
 };
 
 export default SignOut;
