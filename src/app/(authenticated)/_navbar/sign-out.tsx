c528eca6925aff257d9a2dbc64160178c2f8df5d
Jesica <13519011@std.stei.itb.ac.id>
2024-03-04 19:44:32 +0700

    feat: remove toast when signing out

--- src/app/(authenticated)/_navbar/sign-out.tsx
+++ src/app/(authenticated)/_navbar/sign-out.tsx
@@ -1,20 +1,13 @@
 "use client";
 
 import { useMutation } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
 import { signOut } from "next-auth/react";
-import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
 
 const SignOut = () => {
-  const router = useRouter();
   const { mutate, isPending } = useMutation({
-    mutationFn: async () => signOut({ redirect: false }),
-    onSuccess: () => {
-      toast.success("Successfully signed out");
-      router.push("/sign-in");
-    },
+    mutationFn: async () => signOut(),
   });
 
   return (
