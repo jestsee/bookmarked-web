4f24292fccda0c2ca49c70828f5e5cc1dd029650
Jesica <13519011@std.stei.itb.ac.id>
2024-03-05 17:24:21 +0700

    chore: update navbar

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,11 +1,21 @@
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+
 import { LayoutProps } from "@/types/component";
 
 import Navbar from "./navbar";
 
-const AuthenticatedLayout = ({ children }: LayoutProps) => {
+const AuthenticatedLayout = async ({ children }: LayoutProps) => {
+  const session = await getServerSession();
+
+  if (!session || !session.user) {
+    redirect("/sign-in");
+  }
+
   return (
-    <main className="flex h-screen items-center justify-center">
-      <Navbar />
+    // TODO set max width so the navbar won't offside to the right when user using ultrawide monitor
+    <main className="flex h-screen max-w-4xl items-center justify-center">
+      <Navbar user={session.user} />
       <div>{children}</div>
     </main>
   );
--- src/app/(authenticated)/navbar/index.tsx
+++ src/app/(authenticated)/navbar/index.tsx
@@ -1,31 +1,12 @@
-import { LogOut } from "lucide-react";
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-
-import SignOutWrapper from "./sign-out-wrapper";
+import { User } from "../type";
 import UserInfo from "./user-info";
 
-const Navbar = () => {
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger className="fixed right-8 top-8">
-        <UserInfo />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="mt-1">
-        <SignOutWrapper>
-          <DropdownMenuItem>
-            <LogOut className="mr-2 h-4 w-4" />
-            <span>Log out</span>
-          </DropdownMenuItem>
-        </SignOutWrapper>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+interface Props {
+  user: User;
+}
+
+const Navbar = ({ user }: Props) => {
+  return <UserInfo {...{ user }} />;
 };
 
 export default Navbar;
