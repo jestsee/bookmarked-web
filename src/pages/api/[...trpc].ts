a59ee73fc739aab3553696d6cfdd9ef0dde9bca9
Jesica <13519011@std.stei.itb.ac.id>
2024-03-13 20:18:29 +0700

    chore: update trpc dependencies

--- src/pages/api/[...trpc].ts
+++ src/pages/api/[...trpc].ts
@@ -1,17 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { createOpenApiNextHandler } from "trpc-openapi";
 
-import { createContext } from "../../trpc/trpc.context";
-import { appRouter } from "../test-router";
+import { createContext } from "@/app/api/trpc/trpc.context";
+import { appRouter } from "@/server/test-router";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("masok sini gann");
   return createOpenApiNextHandler({
     router: appRouter,
     createContext,
-    responseMeta: null,
     onError: null,
+    responseMeta: null,
   })(req, res);
 };
 
-export { handler as GET, handler as POST };
+export default handler;
