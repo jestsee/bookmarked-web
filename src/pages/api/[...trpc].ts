617ae1b6b1f0a5d3aba1b7bbc948af1fa19347dd
Jesica <13519011@std.stei.itb.ac.id>
2024-03-13 22:04:18 +0700

    chore: custom createContext for open API handler

--- src/pages/api/[...trpc].ts
+++ src/pages/api/[...trpc].ts
@@ -1,13 +1,28 @@
+import { TRPCError } from "@trpc/server";
 import { NextApiRequest, NextApiResponse } from "next";
+import { getToken } from "next-auth/jwt";
 import { createOpenApiNextHandler } from "trpc-openapi";
 
-import { createContext } from "@/app/api/trpc/trpc.context";
 import { appRouter } from "@/server/test-router";
+
+const createContext = async (req: NextApiRequest) => {
+  const token = await getToken({ req });
+
+  if (!token || !token.sub) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "You must be logged in first",
+    });
+  }
+
+  const { picture: image, sub: id, ...rest } = token;
+  return { user: { ...rest, image, id } };
+};
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   return createOpenApiNextHandler({
+    createContext: () => createContext(req),
     router: appRouter,
-    createContext,
     onError: null,
     responseMeta: null,
   })(req, res);
