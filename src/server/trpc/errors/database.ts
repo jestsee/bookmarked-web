76f9b22fa86c4a3e066b1edacb671c36c1b895e2
Jesica <jestsee@gmail.com>
2024-03-16 13:04:32 +0700

    chore: update telegram redirect

--- src/server/trpc/errors/database.ts
+++ src/server/trpc/errors/database.ts
@@ -1,10 +1,12 @@
 import { ErrorMessages } from "./type";
 
-export const databaseErrors: ErrorMessages = {
+export const databaseErrors = {
   "duplicate key value violates unique constraint": {
     // TODO telegram keyword is hardcoded, need to update it if add more account provider later
     connectedAccount_accountId_accountProvider_pk:
-      "This telegram account is already connected",
+      "This telegram account is already connected to other Bookmarked account",
     user_email_unique: "User with this email already exist",
   },
 };
+
+export default databaseErrors as ErrorMessages;
--- src/server/trpc/errors/type.ts
+++ src/server/trpc/errors/type.ts
@@ -1,5 +1,12 @@
+import { databaseErrors } from "./database";
+
 export type ErrorMessages = {
   [key: string]: { [key: string]: string };
 };
 
 export type AtLeastTwoElements<T> = [T, T, ...T[]];
+
+export type CustomDbError = keyof typeof databaseErrors;
+
+export type CustomDbPropertyError =
+  keyof (typeof databaseErrors)[CustomDbError];
