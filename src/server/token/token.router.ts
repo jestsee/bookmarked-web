05b6607722d789711c12162eb1a4ec643968d6a6
Jesica <jestsee@gmail.com>
2024-03-16 11:41:04 +0700

    chore: standardize import using import alias

--- src/server/token/token.router.ts
+++ src/server/token/token.router.ts
@@ -3,10 +3,12 @@ import { publicProcedure, t } from "@/server/trpc/trpc.server";
 import { tokenExchangeHandler } from "./token.controller";
 import { tokenExchangePayload, tokenExchangeResponse } from "./token.schema";
 
-export const tokenRouter = t.router({
+const tokenRouter = t.router({
   tokenExchange: publicProcedure
     .meta({ openapi: { method: "POST", path: "/token" } })
     .input(tokenExchangePayload)
     .output(tokenExchangeResponse)
     .mutation(({ input }) => tokenExchangeHandler(input)),
 });
+
+export default tokenRouter;
