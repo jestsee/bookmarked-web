f9c7788037c92b8cbc3a690a466fedac145a5ed8
Jesica <jestsee@gmail.com>
2024-05-01 21:28:20 +0700

    feat: able to redirect to telegram after signed in with auth provider

--- src/app/(auth)/components/auth-provider-button.tsx
+++ src/app/(auth)/components/auth-provider-button.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
 import { ClientSafeProvider, signIn } from "next-auth/react";
 
 import { Button } from "@/components/ui/button";
@@ -11,13 +12,16 @@ interface Props {
 }
 
 const AuthProviderButton = ({ provider }: Props) => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams?.get("callbackUrl") ?? "/";
   const Icon = AuthIcons[provider.id] ?? `Sign in with ${provider.name}`;
+
   return (
     <Button
       variant="outline"
       className="w-full py-5"
       key={provider.name}
-      onClick={() => signIn(provider.id)}
+      onClick={() => signIn(provider.id, { callbackUrl })}
     >
       <Icon className="h-6 w-6" />
     </Button>
